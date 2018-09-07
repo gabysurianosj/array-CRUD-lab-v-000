d15d0acc7666fc04ec0e3c31cb02163d6eee83a2
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 10:05:18 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  arrays = [1,2,3,4]
-  arrays.push(5)
-  puts arrays.inspect
+  array.push
 end
 
 def add_element_to_start_of_array(array, element)
